040feb0abad385aaf7ec111ec5a5225eb5d87d3a
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2022-12-23 00:12:37 +0000

    Mandatory tasks

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -11,6 +11,7 @@ class Square:
 
         Args:
             size (int): The size of the new square.
+            position (int, int): The position of the new square.
         """
         self.size = size
         self.position = position
@@ -24,7 +25,7 @@ class Square:
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
@@ -40,7 +41,7 @@ class Square:
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.position = value
+        self.__position = value
 
 
     def area(self):
@@ -53,8 +54,8 @@ class Square:
             print("")
             return
 
-        [print("") for i in range(0, self.position[1])]
+        [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.position[0])]
-            [print(" ", end="") for k in range(0, self.__size)]
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
             print("")
